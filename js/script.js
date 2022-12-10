9ad280931edf47176612a51f338677f200178c89
Painkiller3x <116508370+Painkiller3x@users.noreply.github.com>
2022-12-10 14:00:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const Magic = document.querySelector('#g1');
+Magic.addEventListener('click',() => {location.href = "https://painkiller3x.github.io/lr7"});
+const Random = document.querySelector('#g2');
+Random.addEventListener('click',() => {location.href = "https://painkiller3x.github.io/lr8"});
+const Blackjack = document.querySelector('#g3');
+Blackjack.addEventListener('click',() => {location.href = "https://painkiller3x.github.io/lr9"});
+const Slots = document.querySelector('#g4');
+Slots.addEventListener('click',() => {location.href = "https://painkiller3x.github.io/lr10"});
