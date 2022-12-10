816695a23ece608bb590b3e9a81e1603440d2db8
Painkiller3x <116508370+Painkiller3x@users.noreply.github.com>
2022-12-10 13:58:27 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const Magic = document.querySelector('#g1');
-Magic.addEventListener('click',() => {location.replace("https://painkiller3x.github.io/lr7")},{once:true});
-const Random = document.querySelector('#g2');
-Random.addEventListener('click',() => {location.replace("https://painkiller3x.github.io/lr8")},{once:true});
-const Blackjack = document.querySelector('#g3');
-Blackjack.addEventListener('click',() => {location.replace("https://painkiller3x.github.io/lr9")},{once:true});
-const Slots = document.querySelector('#g4');
-Slots.addEventListener('click',() => {location.replace("https://painkiller3x.github.io/lr10")},{once:true});
